bf1ab23c34d3b00029f3395be31cb1a938cd267d
Dave Wu <eyaswoo@gmail.com>
2017-11-09 18:16:00 +0800

    workable smallest

--- action_runner/job_handler.go
+++ action_runner/job_handler.go
@@ -1,18 +1,43 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"legolas/common/models"
 	"log"
+	"os"
+	"os/exec"
+	"strings"
 )
 
 func jobHandler(data string) {
-	log.Println("Get =>")
-	log.Println(data)
+	log.Printf("Get => [ %s ]", data)
 
 	job, err := models.JobFromJson([]byte(data))
 	if err != nil {
 		log.Printf("Unmarshalling failed: %v\n", err)
 	}
 
-	log.Println(job)
+	log.Printf("snippet => {\n%s\n}\n", job.Snippet)
+
+	tempFileName := fmt.Sprintf("%s#%s_snippet.py", job.CaseRunID, job.Name)
+	err = ioutil.WriteFile(tempFileName, []byte(strings.Trim(job.Snippet, "\n ")), 0755)
+	if err != nil {
+		log.Printf("cannopt write snippet to file: %v\n", err)
+	}
+	defer func() {
+		log.Println("remove tempfile:", tempFileName)
+		err := os.Remove(tempFileName)
+		if err != nil {
+			log.Printf("cannot remove temp file: %s: %v\n", tempFileName, err)
+		}
+	}()
+
+	cmd := exec.Command("python", tempFileName)
+	cmdOut, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Printf("cannot get output of process: %v\n", err)
+	}
+
+	log.Printf("output => {\n%s\n}\n", strings.Trim(string(cmdOut), "\n "))
 }
