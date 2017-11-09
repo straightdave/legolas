4e62485ef59c10a04082487d6314902188f0f99d
Dave Wu <eyaswoo@gmail.com>
2017-11-09 18:51:05 +0800

    update

--- action_runner/action_runner.go
+++ action_runner/action_runner.go
@@ -20,27 +20,30 @@ var (
 func main() {
 	flag.Parse()
 
-	// set log to local file
 	logf, err := os.OpenFile(*logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
-		log.Fatalf("Cannot access to log file: %s\n", *logFile)
+		fmt.Printf("Cannot access to log file: %s\n", *logFile)
+		os.Exit(1)
 	}
-	defer logf.Close()
 	log.SetOutput(logf)
 
 	client, err := redis.Dial("tcp", *queueAddress)
 	if err != nil {
 		log.Fatalf("Cannot connect to Redis instance: %s\n", *queueAddress)
 	}
-	defer client.Close()
+
+	var cleanup = func() {
+		logf.Close()
+		client.Close()
+	}
+	defer cleanup()
 
 	// handling ctrl-c/TERM, to exit gracefully
 	c := make(chan os.Signal, 2)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
 		<-c
-		logf.Close()
-		client.Close()
+		cleanup()
 		fmt.Println("legolas action runner is stoppeed.")
 	}()
 
--- action_runner/job_handler.go
+++ action_runner/job_handler.go
@@ -16,27 +16,22 @@ func jobHandler(data string) {
 	job, err := models.JobFromJson([]byte(data))
 	if err != nil {
 		log.Printf("Unmarshalling failed: %v\n", err)
+		return
 	}
-
-	log.Printf("snippet => {\n%s\n}\n", job.Snippet)
 
 	tempFileName := fmt.Sprintf("%s#%s_snippet.py", job.CaseRunID, job.Name)
 	err = ioutil.WriteFile(tempFileName, []byte(strings.Trim(job.Snippet, "\n ")), 0755)
 	if err != nil {
 		log.Printf("cannopt write snippet to file: %v\n", err)
+		return
 	}
-	defer func() {
-		log.Println("remove tempfile:", tempFileName)
-		err := os.Remove(tempFileName)
-		if err != nil {
-			log.Printf("cannot remove temp file: %s: %v\n", tempFileName, err)
-		}
-	}()
+	defer os.Remove(tempFileName)
 
 	cmd := exec.Command("python", tempFileName)
 	cmdOut, err := cmd.CombinedOutput()
 	if err != nil {
 		log.Printf("cannot get output of process: %v\n", err)
+		return
 	}
 
 	log.Printf("output => {\n%s\n}\n", strings.Trim(string(cmdOut), "\n "))
